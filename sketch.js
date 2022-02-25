1a6f530b01f4af147c42d4632c4b949f069ee703
Shravya-Sandeeep <86299610+Shravya-Sandeeep@users.noreply.github.com>
2022-02-25 17:58:48 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,18 +13,11 @@ function setup(){
   canvas = createCanvas(850,400);
   database = firebase.database();
   quiz = new Quiz();
-  quiz.getState();
   quiz.start();
 }
 
 
 function draw(){
   background("pink");
-  if(contestantCount === 2){
-    quiz.update(1);
-  }
-  if(gameState === 1){
-    clear();
-    quiz.play();
-  }
+  
 }
